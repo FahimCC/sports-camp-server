16bfa5597ab43cc920c7722da778f4568b7a90ce
FahimCC <ffnasif@gmail.com>
2023-06-10 15:41:58 +0600

    added 2 route which is set the role admin or instructor

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +10,26 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (!authorization) {
+		return res
+			.status(401)
+			.send({ error: true, message: 'Unauthorized access' });
+	}
+	const token = authorization.split(' ')[1];
+
+	jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+		if (err) {
+			return res
+				.status(401)
+				.send({ error: true, message: 'Unauthorized access' });
+		}
+		req.decoded = decoded;
+		next();
+	});
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ws55k5x.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -53,6 +73,28 @@ async function run() {
 			}
 			user.role = 'student';
 			const result = await userCollection.insertOne(user);
+			res.send(result);
+		});
+		app.patch('/users/admin/:id', async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: 'admin',
+				},
+			};
+			const result = await userCollection.updateOne(filter, updateDoc);
+			res.send(result);
+		});
+		app.patch('/users/instructor/:id', async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: 'instructor',
+				},
+			};
+			const result = await userCollection.updateOne(filter, updateDoc);
 			res.send(result);
 		});
 
